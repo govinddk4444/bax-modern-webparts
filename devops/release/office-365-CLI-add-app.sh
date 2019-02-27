89b1e7b489da97edc398d2a9374721f6d48c24fb
Govind <govind_d@baxter.com>
2019-02-27 21:07:29 +0530

    release bash update

--- devops/release/office-365-CLI-add-app.sh
+++ devops/release/office-365-CLI-add-app.sh
@@ -1,1 +1,12 @@
 SITE_URL=$1
+EMAIL=$2
+PASS=$3
+
+npm i -g @pnp/office365-cli
+o365 version
+
+o365 spo login $SITE_URL --authType password --userName $EMAIL --password $PASS
+
+o365 spo app add --filePath "./_SPFx build/SPFx sppkg/bax-modern-webparts.sppkg" --overwrite
+
+o365 spo app deploy --name bax-modern-webparts.sppkg --skipFeatureDeployment
